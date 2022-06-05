746d426486714fbf9a185a3036884cea9464e7a3
Milan Thapa <milanthapa299@gmail.com>
2022-06-05 12:32:59 +0545

    welcome message and instruction

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import time
 
 
 def quiz():
-    pass
+    print("Welcome to KO HUNCHA LAKHPATI.")
+    time.sleep(3)
+    print(
+        "You'll be asked series of question try to answer the question before "
+        "10 seconds else you'll not get point for that question.")
+    time.sleep(5)
 
 
 if __name__ == "__main__":
